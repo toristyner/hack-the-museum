3140dee600c98ae9f4dc20d993395f9f40c32ac6
kuuurt13 <kurt.stackhouse@jefferson.edu>
2018-05-05 23:45:40 -0400

    server: return location info in list view

--- server/src/modules/museum/services/formatter.js
+++ server/src/modules/museum/services/formatter.js
@@ -1,6 +1,6 @@
 import { reduceProps, mapProps, sortDesc } from '../../../shared/formatter'
 
-const simpleList = ['ObjectID', 'Title', 'Artist', 'Thumbnail']
+const simpleList = ['ObjectID', 'Title', 'Artist', 'Thumbnail', 'Location']
 const list = [...simpleList, 'genres']
 const detail = [
   'ObjectID',
@@ -12,9 +12,25 @@ const detail = [
   'music'
 ]
 
+const mapGallery = gallery => ({
+  Gallery: gallery.Gallery,
+  GalleryShort: gallery.GalleryShort
+})
+
+const mapSimpleList = data =>
+  data.map(d =>
+    simpleList.reduce(
+      (prev, prop) => ({
+        ...prev,
+        [prop]: prop === 'Location' ? mapGallery(d[prop]) : d[prop]
+      }),
+      {}
+    )
+  )
+
 export default {
   list: data => mapProps(data, list),
-  simpleList: data => mapProps(data, simpleList),
+  simpleList: mapSimpleList,
   detail: data => reduceProps(data, detail),
   sortSongs: songs => songs.sort(sortDesc('popularity'))
 }
